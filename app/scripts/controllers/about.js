96e4084ba586fb5755f2174d58ca841db204cb25
Luis Arce <amigox100pre111@gmail.com>
2016-10-03 23:17:16 -0400

    Initialized an angular project

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exampleAngularApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the exampleAngularApp
+ */
+angular.module('exampleAngularApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name exampleAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the exampleAngularApp
+ */
+angular.module('exampleAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
